dc550a3ff362df32d8910385b72b44ad6dc6773f
Daniel Fonseca Alves <dalrafgmail.com>
2017-08-21 13:17:16 -0300

    iniciando template

--- graficos/views.py
+++ graficos/views.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+from django.shortcuts import render
+
+import os
+
 # Create your views here.
+
+
+def default(request):
+    path="/home/daniel/csv"  # insert the path to your directory   
+    cvslist =os.listdir(path)   
+    return render(request,'listar.html', {'csvs': cvslist})

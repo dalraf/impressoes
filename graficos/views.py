6bab085c0d692891ba70e0d1dd7883cebba87684
Daniel Fonseca Alves <dalrafgmail.com>
2017-08-22 13:43:26 -0300

    Criado variavel de path

--- graficos/views.py
+++ graficos/views.py
@@ -19,15 +19,17 @@ import csv
 
 # Create your views here.
 
+csv_diretorio="/home/daniel/csv"
+
 
 def default(request):
-    path="/home/daniel/csv"  # insert the path to your directory
+    path=csv_diretorio
     cvslist = [re.sub(r'^papercut-print-log-([0-9-]+).csv',r'\1',w) for w in os.listdir(path)]
     return render(request,'listar.html', {'csvs': cvslist})
 
 
 def gerar(request, detalhe):
-    csvfile = '/home/daniel/csv/papercut-print-log-' + detalhe + '.csv'
+    csvfile = csv_diretorio + '/papercut-print-log-' + detalhe + '.csv'
     csvfileopen = csv.DictReader(open(csvfile).readlines()[1:], delimiter=str(u','),dialect=csv.excel)
     csvdict = []
     for line in csvfileopen:
